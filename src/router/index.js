45854528c73b5d9f30070d960c654404e100b7bb
靳晓宇 <7734304+Jin816@user.noreply.gitee.com>
2020-08-25 20:12:58 +0800

    靳晓宇提交1

--- src/router/index.js
+++ src/router/index.js
@@ -13,26 +13,18 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-<<<<<<< HEAD
     component: () => import( '../views/About.vue')
   },
   {
     path: '/Mai',
     name: 'Mai',
     component: () => import( '../views/Mai.vue')
-  }
-=======
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/Decoration',
     name: 'Decoration',
     component: () => import('../views/Decoration.vue')
   },
->>>>>>> 720116f3d6aeda5ff6ed425e15b15fa980475fff
 ]
 
 const router = new VueRouter({
